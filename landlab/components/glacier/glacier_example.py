36c1148cee2d0f7584f029ebc543f861229cc374
jiaweih <jiaweihe@D-69-91-145-46.dhcp4.washington.edu>
2015-03-03 13:25:13 -0800

    add plotting

--- landlab/components/glacier/glacier_example.py
+++ landlab/components/glacier/glacier_example.py
@@ -6,6 +6,16 @@ from landlab import RasterModelGrid
 
 def main():
 
+	'''
+	B: bed elevation 
+	b_dot:
+	dt: time interval
+	t_STOP: ending time for modeling
+	t: starting time for modeling
+	dx: node spacing (dx = dy)
+	nx: number of columns of nodes
+	ny: number of rows of nodes
+	'''
 	input_file = 'mb4_spin1.mat'
 	mat = io.loadmat(input_file)
 	B = mat['B']
@@ -15,20 +25,15 @@ def main():
 	nx = np.int_(mat['nx'][0,0])
 	ny = np.int_(mat['ny'][0,0])
 	
-
-	t_STOP = 0.1        ### 1000
+	t_STOP = 10        ### 1000
 	dt = 0.08333
 	t = 0	
 
-	grid = RasterModelGrid(ny,nx,dx)
-
+	### put input data in a dictionary
 	B,b_dot,S = flatten(B,b_dot)
-
-
-
 	dictionary = {'S':S,'B':B,'b_dot':b_dot,'dt':dt,'t_STOP':t_STOP,'t':t,'dx':dx,'nx':nx,'ny':ny}
-	gla = glacier.Glacier(grid,dictionary)
-
+	grid = RasterModelGrid(nx,ny,dx)
+	gla = glacier.Glacier(grid,dictionary) 
 	gla.recursive_steps()
 
 	S_map = gla.grid['node']['ice_elevation'] 
@@ -38,7 +43,12 @@ def main():
 	np.savetxt('B_map.txt',B_map)
 	np.savetxt('I_map.txt',I_map)
 
+	plt.figure(figsize=(10,8))
+	plt.imshow(S_map)
+	plt.savefig('S_map.pdf')
+
 def flatten(B,b_dot):
+	### flatten two dimensional matrix
 	B = B.T.flatten()
 	B[np.isnan(B)] = 0
 	S = B
